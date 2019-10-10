5ba0bfd087ececd64f60433bf9c84445eda5ad09
lovejoyy <54951696+lovejoyy@users.noreply.github.com>
2019-10-10 12:22:18 -0500

    Add files via upload

---
+++ scripts/myScript/myScript.js
@@ -0,0 +1,7 @@
+function myFunction() {
+  var name = prompt("Hello! Whats your name?");
+  if (name != null) {
+    alert("Hello" + name + "! Hope you are having a wonderful day today! Remember to always smile(:");
+	console.log(name);
+  }
+}
