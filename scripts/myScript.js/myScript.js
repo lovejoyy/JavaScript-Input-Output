30bb8a3d074c94327c480cb798eaf3b89ad23ef8
lovejoyy <54951696+lovejoyy@users.noreply.github.com>
2019-10-10 12:21:50 -0500

    Delete myScript.js

--- scripts/myScript.js/myScript.js
+++
@@ -1,7 +0,0 @@
-function myFunction() {
-  var name = prompt("Hello! Whats your name?");
-  if (name != null) {
-    alert("Hello" + name + "! Hope you are having a wonderful day today! Remember to always smile(:");
-	console.log(name);
-  }
-}
